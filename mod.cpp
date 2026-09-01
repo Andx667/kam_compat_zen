19734bd50fe26f98588a271f48458cb94eae5992
Andx667 <andx667@protonmail.com>
2026-09-01 21:01:07 +0200

    add images

--- mod.cpp
+++ mod.cpp
@@ -2,3 +2,7 @@ name = "KAM Compat ZEN [Beta] v0.0.0";
 overview = "Compatibility patch between KAT - Advanced Medical (KAM) and Zeus Enhanced (ZEN).";
 hideName = 0;
 hidePicture = 0;
+picture = "logo_ca.paa";
+logo = "logo_ca.paa";
+logoOver = "logo_ca.paa";
+tooltip = "KAM Compat ZEN";
